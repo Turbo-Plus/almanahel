50a97c9cfa3c95a82e563bd0868be8b4b501bb0d
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-05-25 00:59:42 +0300

    catr

--- js/cart.js
+++ js/cart.js
@@ -1,83 +1,116 @@
 'use strict';
-
+let th;
+let tr;
+let remov;
 let total = 0;
 let quantity = document.getElementsByClassName('qunt');
 let allCart;
 let table = document.getElementById('tableCart');
-if (localStorage.cart) {
-  allCart = JSON.parse(localStorage.cart);
-  console.log('allCart', allCart);
-  for (let i = 0; i < allCart.length; i++) {
-    let tr = document.createElement('tr');
-    table.appendChild(tr);
-    let td = document.createElement('td');
-    tr.appendChild(td);
-    let img = document.createElement('img');
-    tr.appendChild(img);
-    img.src = allCart[i].source;
-    // console.log('cartsource', allCart[i].source);
-    img.alt = allCart[i].name;
+function rendercart() {
+  if (localStorage.cart) {
+    allCart = JSON.parse(localStorage.cart);
+    console.log('allCart', allCart);
+    for (let i = 0; i < allCart.length; i++) {
+      tr = document.createElement('tr');
+      tr.setAttribute('id', i);
+      table.appendChild(tr);
+      let td = document.createElement('td');
+      tr.appendChild(td);
+      let img = document.createElement('img');
+      tr.appendChild(img);
+      img.src = allCart[i].source;
+      // console.log('cartsource', allCart[i].source);
+      img.alt = allCart[i].name;
 
-    quantity = document.createElement('input'); // create quantity input for the user
-    tr.appendChild(quantity);
-    quantity.setAttribute('type', 'number');
-    quantity.setAttribute('placeholder', 'quantity');
-    quantity.setAttribute('value', '1');
-    quantity.setAttribute('class', 'qunt');
+      quantity = document.createElement('input'); // create quantity input for the user
+      tr.appendChild(quantity);
+      quantity.setAttribute('type', 'number');
+      quantity.setAttribute('placeholder', 'quantity');
+      quantity.setAttribute('value', '1');
+      quantity.setAttribute('class', 'qunt');
 
-    let remov = document.createElement('button');
-    tr.appendChild(remov);
-    remov.innerHTML = 'delete';
-    td.textContent = `${allCart[i].name} ${allCart[i].price}JD`;
-    remov.addEventListener('click', removrow);
-    // eslint-disable-next-line no-inner-declarations
-    function removrow() {
-      tr.remove(i);
-      // الجلطة  by Hamza
-      //   console.log(localStorage.cart);
-      //   localStorage.removeItem('cart' ,localStorage[i]);
-      let cart = JSON.parse(localStorage.getItem('cart'));
-      cart.splice(i, 1);
-      localStorage.setItem('cart', JSON.stringify(cart));
+      remov = document.createElement('button');
+      remov.setAttribute('class', 'removebutton');
+      tr.appendChild(remov);
+      remov.innerHTML = 'delete';
+      td.textContent = `${allCart[i].name} ${allCart[i].price}JD`;
+     
+      // eslint-disable-next-line no-inner-declarations
+
+      th = document.createElement('th');
+      let totalRow = document.createElement('tr'); // calculate
+      table.appendChild(totalRow);
+      totalRow.appendChild(th);
+      // removrow();
     }
+    
+    // console.log(quantity.value);
+
   }
 
-  // console.log(quantity.value);
-
 }
-
-
-
+// rendercart();
 //////////// books Total
-
-function booksTotal(){
-  let th = document.createElement('th');
-  let totalRow = document.createElement('tr'); // calculate
+// remov.getElementsByClassName('removebutton');
+// remov.addEventListener('click', removrow);
+function removrow() {
+  for (let i = 0; i <allCart.length; i++) {
+  tr.remove(i);
+  // الجلطة  by Hamza
+  //   console.log(localStorage.cart);
+  //   localStorage.removeItem('cart' ,localStorage[i]);
+  let cart = JSON.parse(localStorage.getItem('cart'));
+  cart.splice(i, 1);
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+}
+function booksTotal() {
+  // let th = document.createElement('th');
+  // let totalRow = document.createElement('tr'); // calculate
+  // table.appendChild(totalRow);
+  // totalRow.appendChild(th);
   for (let i = 0; i < allCart.length; i++) {
 
-    table.appendChild(totalRow);
-    totalRow.appendChild(th);
-    total= total + (parseInt(allCart[i].price) * quantity.value);
-
+    total = total + (parseInt(allCart[i].price) * quantity.value);
+    console.log(quantity.value);
   }
   th.textContent = `Total ${total}`;
 }
-booksTotal();
+// booksTotal();
 
 ////////////////////////////////////////////// change q number
 let quantityChange = document.getElementsByClassName('qunt');
 for (let i = 0; i < quantityChange.length; i++) {
   let input = quantityChange[i];
-  input.addEventListener('change' , inputChanged);
+  input.addEventListener('change', inputChanged);
 }
-function inputChanged(event){
+function inputChanged(event) {
   let input = event.target;
-  if(isNaN(input.value) || input.value <= 0){
+  if (isNaN(input.value) || input.value <= 0) {
     input.value = 1;
   }
-  console.log(input.value);
+  // booksTotal();
+  // console.log(input.value);
   console.log('total', total);
 }
 
 
+function setProduct() {
+  let productdata = JSON.stringify(localStorage.cart);
+  console.log(productdata);
+  localStorage.setItem('cart', productdata);
+}
 
+let allproduct =[];
+
+function gitProduct() {
+  let stringdata = localStorage.getItem('cart');
+  allproduct = JSON.parse(stringdata);
+  if (stringdata) {
+    allproduct= JSON.parse(stringdata)
+    // localStorage.cart = allproduct;
+    console.log(allproduct);
+  }
+
+}
+gitProduct() ;
